a6c1c9b59a15c7378cb8e93ac0fd4a47cfea281f
rydinmyerson <simon.myerson@gmail.com>
2022-06-02 14:45:15 +0100

    animate icon links

--- scripts/animate-icon-links.js
+++ scripts/animate-icon-links.js
@@ -1,1 +1,19 @@
+function spotlightNextIcon() {
+    oldSpotlight = document.querySelector(".spotlight");
+    newSpotlight = null;
 
+    if (oldSpotlight) {
+        oldSpotlight.classList.remove('spotlight');
+        newSpotlight = oldSpotlight.nextElementSibling;
+    }
+
+    if (newSpotlight) {
+        newSpotlight.classList.add('spotlight');
+    } else {
+        document.querySelector(".icon-links").firstElementChild.classList.add('spotlight');
+    }
+}
+
+function startTimer() {
+    setInterval(spotlightNextIcon,2000);
+}
